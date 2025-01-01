bd9f8f53ad007db99da81c97355a105a1b9744ab
CoreyStagner <cresc0428@gmail.com>
2025-01-01 18:11:13 -0500

    Working on implementing Auth validation from sentinal.

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,32 @@
 import globalStyles from '../../styles/global.js';
+// import { useEffect, useState } from 'react';
 
 function Layout(props) {
+  // TODO: work on getting this to work with gantt_tracker app so that we can use a central auth server. Will also need to set up roles for each app.
+  // const [isUserValid, setIsUserValid] = useState(false);
+  // const [isValidationComplete, setIsValidationComplete] = useState(false);
+  // useEffect(() => {
+  //   // validate user
+  //   const validateUser = async () => {
+  //     const isValid = await fetch(
+  //       process.env.SENTINAL_AUTHENTICATION_URL ||
+  //         'https://localhost:3041/api/auth/sso/validate'
+  //     );
+  //     console.log('here', isValid);
+  //     setIsUserValid(isValid);
+  //     setIsValidationComplete(true);
+  //   };
+  //   validateUser();
+  // }, []);
+
+  // if (!isValidationComplete) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (!isUserValid) {
+  //   return <div>Unauthorized</div>;
+  // }
+
   return (
     <div className="page-layout">
       {props.children}
