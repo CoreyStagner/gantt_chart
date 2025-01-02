cc6bf0fd80bda4c2605dc615f84f13f3f1426f2d
CoreyStagner <cresc0428@gmail.com>
2025-01-02 09:34:12 -0500

    Working on implementing Auth validation from sentinal.

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,31 +1,65 @@
 import globalStyles from '../../styles/global.js';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Layout(props) {
   // TODO: work on getting this to work with gantt_tracker app so that we can use a central auth server. Will also need to set up roles for each app.
-  // const [isUserValid, setIsUserValid] = useState(false);
-  // const [isValidationComplete, setIsValidationComplete] = useState(false);
-  // useEffect(() => {
-  //   // validate user
-  //   const validateUser = async () => {
-  //     const isValid = await fetch(
-  //       process.env.SENTINAL_AUTHENTICATION_URL ||
-  //         'https://localhost:3041/api/auth/sso/validate'
-  //     );
-  //     console.log('here', isValid);
-  //     setIsUserValid(isValid);
-  //     setIsValidationComplete(true);
-  //   };
-  //   validateUser();
-  // }, []);
+  const [isUserValid, setIsUserValid] = useState(false);
+  const [isValidationComplete, setIsValidationComplete] = useState(false);
 
-  // if (!isValidationComplete) {
-  //   return <div>Loading...</div>;
-  // }
+  // validate user
+  const validateUser = async () => {
+    // const isValid = async () => {
+    console.log('\n\n\n\n\n\n\n\nTesting validation\n\n\n\n\n\n\n');
+    const response = await fetch(
+      process.env.SENTINAL_AUTHENTICATION_URL ||
+        'https://localhost:3041/api/auth/sso/validate',
+      {
+        method: 'POST',
+        key: 'Access-Control-Allow-Origin',
+        value: 'https://localhost:3041',
+      }
+    )
+      .then((r) => r)
+      .then((r2) => {
+        console.log('r2', r2);
+        return r2;
+      });
 
-  // if (!isUserValid) {
-  //   return <div>Unauthorized</div>;
-  // }
+    return response;
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let isValid = await validateUser()
+          .then((r) => r.json())
+          .then((r2) => {
+            return r2;
+          });
+
+        if (!isValid || isValid.toUpperCase() === 'UNAUTHORIZED') {
+          setIsUserValid(false);
+        } else {
+          // TODO: make sure the cookie exists and has this value else set that value.
+          setIsUserValid(isValid);
+        }
+        setIsValidationComplete(true);
+      } catch (e) {
+        console.error(e);
+        setIsUserValid(false);
+        setIsValidationComplete(true);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (!isValidationComplete) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isUserValid) {
+    return <div>Unauthorized</div>;
+  }
 
   return (
     <div className="page-layout">
